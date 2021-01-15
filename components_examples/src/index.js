dc149df328b5ce2caa5d89f0e37a34c7a8e3d53d
chayanne19 <chayannepaulino@gmail.com>
2021-01-14 23:04:31 -0500

    new props syntax children component

--- components_examples/src/index.js
+++ components_examples/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 
 
 // Simple Component
@@ -8,27 +10,37 @@ const App = () => {
     return (
 
         <div className="ui container comments">
-            <CommentDetail 
-             author="Milton" 
-            timeAt="10:00PM" 
-            comment="Nice job" 
-            img='https://source.unsplash.com/random'
-            />
 
-            <CommentDetail
-             author="Alex" 
-             timeAt="7:00PM" 
-             comment="Cool" 
-            img='https://source.unsplash.com/random'
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                author="Milton" 
+                timeAt="10:00PM" 
+                comment="Nice job" 
+                img='https://source.unsplash.com/random'
+                />
+            </ApprovalCard>
 
-            <CommentDetail 
-            author="Chayanne" 
-            timeAt="5:30PM" 
-            comment="Awsome" 
-            img='https://source.unsplash.com/random'
-            />
+            
+            <ApprovalCard>
+                <CommentDetail
+                author="Alex" 
+                timeAt="7:00PM" 
+                comment="La Vida Es Una" 
+                img='https://source.unsplash.com/random'
+                />
+            </ApprovalCard>
 
+       
+            <ApprovalCard>
+                <CommentDetail 
+                author="Chayanne" 
+                timeAt="5:30PM" 
+                comment="Awsome" 
+                img='https://source.unsplash.com/random'
+                />
+            </ApprovalCard>
+
+    
         </div>
     );
 };
