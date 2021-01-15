41946376cd6a28099e9221255def7c69174da874
chayanne19 <chayannepaulino@gmail.com>
2021-01-14 22:39:08 -0500

    props intro and practice

--- components_examples/src/index.js
+++ components_examples/src/index.js
@@ -8,9 +8,27 @@ const App = () => {
     return (
 
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail 
+             author="Milton" 
+            timeAt="10:00PM" 
+            comment="Nice job" 
+            img='https://source.unsplash.com/random'
+            />
+
+            <CommentDetail
+             author="Alex" 
+             timeAt="7:00PM" 
+             comment="Cool" 
+            img='https://source.unsplash.com/random'
+            />
+
+            <CommentDetail 
+            author="Chayanne" 
+            timeAt="5:30PM" 
+            comment="Awsome" 
+            img='https://source.unsplash.com/random'
+            />
+
         </div>
     );
 };
