cea161434f1ff57601ded95d0ee00a12708feca2
chayanne19 <chayannepaulino@gmail.com>
2021-01-14 22:38:26 -0500

    props intro

--- components_examples/src/CommentDetail.js
+++ components_examples/src/CommentDetail.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+
+    console.log(props);
 
     return (
         <div className="comment">
-
+        
         <a href="/" className="avatar">                   
-            <img alt="avatar" src='https://source.unsplash.com/random' />
+            <img alt="avatar" src={props.img} />
         </a>
 
         <div className="content">
             <a href="/" className="author">
-                Chayanne
+                {props.author}
             </a>
             <div className="metadata">
-                <span className="date">Today at 6:00PM</span>
+                <span className="date"> {props.timeAt}</span>
             </div>
-            <div className="text">Nice blog post!</div>
+            <div className="text">{props.comment}</div>
         </div>
     </div>
 
