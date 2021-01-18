b8bf522349e10ca503bb07ea03f6fa12e1e3f5ad
chayanne19 <chayannepaulino@gmail.com>
2021-01-18 14:24:28 -0500

    props practice

--- practice/basic_example/src/index.js
+++ practice/basic_example/src/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './Navbar';
 import Hello from './Hello';
+import Message from './Message';
+import MessageBox from './MeessageBox';
 
 const App = () => {
 
 
     return (
         <div className="navebar">
-            <Hello />
+         
             <Navbar />
+
+            <Hello />
+
+            <MessageBox>
+                <Message 
+                    title ="Hi"
+                    messageContent = "this is a testes for props"
+                    />
+                             
+            </MessageBox>    
+
+            <MessageBox>
+                <Message 
+                    title ="Hi Mike"
+                    messageContent = "What's up"
+                    />
+                             
+            </MessageBox>    
+       
             
         </div>
 
