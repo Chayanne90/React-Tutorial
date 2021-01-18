ca987c0c4120d1948b199cc237821dc6c2859670
chayanne19 <chayannepaulino@gmail.com>
2021-01-18 16:38:35 -0500

    intro to class components and state

--- practice/seasons/src/index.js
+++ practice/seasons/src/index.js
@@ -2,13 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
+/*
 const App = () => {
+
+
     return (
         
-        <div>Hello</div>
+        <div>Latitude:</div>
     );
-};
+};*/
+
+
+/* CLASS component */
+class App extends React.Component {
+
+    /* This constructor method is the first one to be call
+     every time an instance of the App comment is created 
+    */
+    constructor(props) {
+        super(props);
+
+        // This is the only time you do the direct assignment
+        // to this.state, becasue is inside the constructor method
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                /* THIS IS HOW TO setState */
+                this.setState({ lat: position.coords.latitude });
+
+                // DOT NOT DO THIS 
+                /* this.state.lat = position.coords.latitude */
+            },
+            (err) => console.log(err)
+        );
+
+    }
+
+    // render() is mandatory when crating a class component
+    render() {
+        
+        return(
+            <div>Latitude: {this.state.lat}</div>
+        );
+    }
+}
 
 
 
