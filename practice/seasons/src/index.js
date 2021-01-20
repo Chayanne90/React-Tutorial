89253be66f9351fe4b31b7892288eec5219de347
chayanne19 <chayannepaulino@gmail.com>
2021-01-19 20:20:19 -0500

    state

--- practice/seasons/src/index.js
+++ practice/seasons/src/index.js
@@ -24,26 +24,25 @@ class App extends React.Component {
 
         // This is the only time you do the direct assignment
         // to this.state, becasue is inside the constructor method
-        this.state = { lat: null };
+        this.state = { lat: null, long: null };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 /* THIS IS HOW TO setState */
-                this.setState({ lat: position.coords.latitude });
+                this.setState({ lat: position.coords.latitude, long: position.coords.longitude });
 
                 // DOT NOT DO THIS 
                 /* this.state.lat = position.coords.latitude */
             },
             (err) => console.log(err)
         );
-
     }
 
     // render() is mandatory when crating a class component
     render() {
         
         return(
-            <div>Latitude: {this.state.lat}</div>
+            <div>Latitude: {this.state.lat} Llongitude: {this.state.long}</div>
         );
     }
 }
