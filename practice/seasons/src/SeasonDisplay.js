c145620c54e2a66579109fae20e1abbc08de2705
chayanne19 <chayannepaulino@gmail.com>
2021-01-21 23:48:23 -0500

    passing states as props

--- practice/seasons/src/SeasonDisplay.js
+++ practice/seasons/src/SeasonDisplay.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 
+const getSeason = (lat, month) => {    
 
-const SeasonDisplay = () => {
+    if(month > 2 && month <9) {
+        lat > 0 ? 'summer' : 'winter';
+    }
+
+}
+
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth);
     return (
-        <div>Season Display!</div>
+        <div>Season Display : {props.lat}</div>
     );
 };
 
