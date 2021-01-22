142991229b785fba8d8f473aa36029dd0f8234c1
chayanne19 <chayannepaulino@gmail.com>
2021-01-22 00:09:22 -0500

    SeasonDisplay component

--- practice/seasons/src/SeasonDisplay.js
+++ practice/seasons/src/SeasonDisplay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+/* Get season base on what month you are currently are */
 const getSeason = (lat, month) => {    
 
     if(month > 2 && month <9) {
@@ -8,16 +9,23 @@ const getSeason = (lat, month) => {
     } else {
         return lat > 0 ? 'Winter' : 'Summer';
     }
-
 }
 
+/* season dispaly message  */
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth);
 
-    console.log(season);
+    const text = season === 'Winter' ? 'Burr, it is Chilly' : 'Lets hit the Beach';
+
+    const icon = season === 'Winter' ? 'snowflake' : 'sun';
+
     return (
-        <div>Season Display : {props.lat}</div>
+        <div>
+            <i className={`${icon} icon`}/>
+            <h1>{text}</h1>    
+            <i className={`${icon} icon`}/>
+        </div>
     );
 };
 
