245e9677980da68d96187a95d3cdb4c934dad117
chayanne19 <chayannepaulino@gmail.com>
2021-01-27 00:29:04 -0500

    creating an api file here we handle everything realted to axios

--- practice/pictures/src/api/unsplash.js
+++ practice/pictures/src/api/unsplash.js
@@ -1,4 +1,15 @@
 import axios from 'axios';
 
 
+export default axios.create({
+
+  baseURL:'https://api.unsplash.com',
+  headers: {
+    Authorization: 'Client-ID fvr_wnAHZYuJF15YQ0Xkd9IMG6T1T7826i59WO0it9w'
+  }
+});
+
+
+
+
 
